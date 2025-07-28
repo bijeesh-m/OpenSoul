f98201d0633ca1bfbb4309c07d7995e0d993993d
Bijeesh <bijeeshh.m@gmail.com>
2025-07-28 21:38:25 +0530

    fix:fixed the navigations

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -4,28 +4,37 @@ const { verifyToken } = require("../middlewares/authMiddleware");
 const upload = require("../middlewares/multer");
 const router = express.Router();
 
-// router.use(verifyToken);
 
 router
-    .route("/confession-group")
-    .post(upload.single("bgImage"), adminController.createConfessionGroup)
-    .get(adminController.getAllConfessionGroups);
+  .route("/confession-group", verifyToken)
+  .post(upload.single("bgImage"), adminController.createConfessionGroup)
+  .get(adminController.getAllConfessionGroups);
 
 router
-    .route("/confession-group/:id")
-    .get(adminController.getConfessionGroup)
-    .put(upload.single("bgImage"), adminController.updateConfessionGroup)
-    .delete(adminController.deleteConfessionGroup);
+  .route("/confession-group/:id", verifyToken)
+  .get(adminController.getConfessionGroup)
+  .put(upload.single("bgImage"), adminController.updateConfessionGroup)
+  .delete(adminController.deleteConfessionGroup);
 
-router.route("/confession-group/:id/add-member").put(adminController.addMember);
+router
+  .route("/confession-group/:id/add-member", verifyToken)
+  .put(adminController.addMember);
 
-router.get("/confessions/:groupId", adminController.getGroupConfessions);
-router.delete("/confession-group/:groupId/confession/:confessionId", adminController.deleteConfession);
+router.get(
+  "/confessions/:groupId",
+  verifyToken,
+  adminController.getGroupConfessions
+);
+router.delete(
+  "/confession-group/:groupId/confession/:confessionId",
+  verifyToken,
+  adminController.deleteConfession
+);
 
-router.get("/student", adminController.getAllStudents);
-router.get("/student/:id", adminController.getStudentById);
-router.post("/student", adminController.createStudent);
-router.put("/student/:id", adminController.updateStudent);
-router.delete("/student/:id", adminController.deleteStudent);
+router.get("/student", verifyToken, adminController.getAllStudents);
+router.get("/student/:id", verifyToken, adminController.getStudentById);
+router.post("/student", verifyToken, adminController.createStudent);
+router.put("/student/:id", verifyToken, adminController.updateStudent);
+router.delete("/student/:id", verifyToken, adminController.deleteStudent);
 
 module.exports = router;
