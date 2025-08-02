5b943bd98eb11ccf0eb418ea8ef3ff11518e9fbf
Bijeesh <bijeeshh.m@gmail.com>
2025-08-02 11:27:33 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ require("dotenv").config();
 app.use(express.json());
 // app.use(cors());
 // app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(cookieParser());
 
